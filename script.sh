f2ed949dc636b0714ed5b3661d974102d8fd578e
Andre <andredornas299@gmail.com>
2016-11-20 09:52:47 -0200

    script para rodar os terminais

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
-sudo fuser -k 7777/tcp
-sudo fuser -k 6768/tcp
-sudo fuser -k 5555/tcp
-sudo fuser -k 5554/tcp
-sudo fuser -k 4444/tcp
-sudo fuser -k 9999/tcp
-sudo fuser -k 3333/tcp
+#kill ports
+fuser -k 7777/tcp
+fuser -k 6768/tcp
+fuser -k 5555/tcp
+fuser -k 5554/tcp
+fuser -k 4444/tcp
+fuser -k 9999/tcp
+fuser -k 3333/tcp
+
+cp config ~/.config/terminator/config
+terminator -l redes
